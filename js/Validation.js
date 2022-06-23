a8f7b3ac11e96aa0387b967cccbb6f1c6c8ddcf8
ashishmashal <ashishmashal5@gmail.com>
2022-06-23 22:39:36 +0530

    Added:Header Added For Home Page Of AddressBook.

--- js/Validation.js
+++ js/Validation.js
@@ -111,14 +111,6 @@ const createAddressBook = () => {
     addressBookData.zipcode = getInputValueById('#zipcode');
     return addressBookData;
 }
-const getSelectedValues = (propertyValue) => {
-    let allItems = document.querySelectorAll(propertyValue);
-    let selItems = [];
-    allItems.forEach(item => {
-        if (item.checked) selItems.push(item.value);
-    });
-    return selItems;
-}
 
 const getInputValueById = (id) => {
     let value = document.querySelector(id).value;
